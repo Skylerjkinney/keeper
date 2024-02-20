93d0a9f05380e0ea39baccddbbacd31ebc26356d
Skyler Kinney <Skylerjkinney@gmail.com>
2024-02-20 15:38:07 -0700

    Profiles are ready to be finished then onto vaults

--- server/Controllers/AccountController.cs
+++ server/Controllers/AccountController.cs
@@ -7,10 +7,12 @@ public class AccountController : ControllerBase
   private readonly AccountService _accountService;
   private readonly Auth0Provider _auth0Provider;
 
-  public AccountController(AccountService accountService, Auth0Provider auth0Provider)
+  private readonly VaultService vaultService;
+  public AccountController(AccountService accountService, Auth0Provider auth0Provider, VaultService vaultService)
   {
     _accountService = accountService;
     _auth0Provider = auth0Provider;
+    this.vaultService = vaultService;
   }
 
   [HttpGet]
@@ -37,6 +39,22 @@ public class AccountController : ControllerBase
       string userEmail = userInfo.Email;
       Account account = _accountService.Edit(editData, userEmail);
       return Ok(account);
+    }
+    catch (Exception error)
+    {
+      return BadRequest(error.Message);
+    }
+  }
+  [HttpGet("vaults")]
+  [Authorize]
+  public async Task<ActionResult<List<Vault>>> GetAccountVaults()
+  {
+    try
+    {
+      Account userInfo = await _auth0Provider.GetUserInfoAsync<Account>(HttpContext);
+      string accountId = userInfo.Id;
+      List<Vault> vaults = vaultService.GetMyVaults(accountId);
+      return Ok(vaults);
     }
     catch (Exception error)
     {
